89fa664b0655eca96169b3cf2484b20b25fcbb0e
sparsick <sparsick@web.de>
2020-05-04 18:48:18 +0200

    init project skeleton

---
+++ src/main/java/com/github/sparsick/mavn/extension/docker/push/DockerPushExtension.java
@@ -0,0 +1,4 @@
+package com.github.sparsick.mavn.extension.docker.push;
+
+public class DockerPushExtension {
+}
---
+++ src/test/java/com/github/sparsick/maven/extension/docker/push/DockerPushIT.java
@@ -0,0 +1,22 @@
+package com.github.sparsick.maven.extension.docker.push;
+
+import com.soebes.itf.jupiter.extension.MavenIT;
+import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MavenIT
+public class DockerPushIT {
+
+    @MavenTest
+    void no_extension_is_set(MavenExecutionResult result){
+
+        assertThat(true).isTrue();
+    }
+
+    @MavenTest
+    void extension_is_set(MavenExecutionResult result){
+        assertThat(true).isTrue();
+    }
+}
