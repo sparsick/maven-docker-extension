22db83c220af901c55b644cbd36a7a666aa39a24
sparsick <sparsick@web.de>
2020-06-15 21:45:26 +0200

    extensions exists, also docker maven plugin but no goal deploy

--- src/test/java/com/github/sparsick/maven/extension/docker/push/DockerPushIT.java
+++ src/test/java/com/github/sparsick/maven/extension/docker/push/DockerPushIT.java
@@ -86,7 +86,23 @@ public class DockerPushIT {
         Assertions.assertThat(repositories).asString().doesNotContain("user/demo2");
     }
 
+    @MavenTest(goals = "install") //TODO fix port is bad
+    void extension_is_set_docker_plugin_exists_but_no_deploy_goal(MavenExecutionResult result){
+        assertThat(result).isSuccessful();
+
+        Assertions.assertThat(assertThat(result).log().info())
+                .contains("maven-docker-push-extension is active",
+                        "Found io.fabric8:docker-maven-plugin:push")
+                .doesNotContain("Starting pushing docker images...");
+
+        Object repositories = Unirest.get("http://localhost:5001/v2/_catalog")
+                .asJson()
+                .mapBody(node -> node.getObject().get("repositories"));
+        Assertions.assertThat(repositories).asString().doesNotContain("user/demo2");
+    }
+
 
 
     // todo failure during pushing in extension
+    // todo push is binded to other goal
 }
